2f11353efb2ae570ed864427d83c1407c66a964d
Robert Dalton <robert@Roberts-MacBook-Pro.local>
2021-01-11 11:38:28 -0800

    Added new tests, fixed multiple bugs in Extract.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pretty-pipeline",
-    version="0.0.1",
+    version="0.0.3",
     author="Rob Dalton",
     description="Package for creating ETL pipelines.",
     long_description=long_description,
